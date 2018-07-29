5e2579a228147d16942bd9840b79441104d51104
Gaurav <gdhameeja@gmail.com>
2018-07-29 21:42:17 +0530

    Yes Yes Yes

--- cv-gui/paint/paint.py
+++ cv-gui/paint/paint.py
@@ -7,28 +7,38 @@ class Canvas:
         self.name = name
         self.window = cv2.namedWindow(name)
         self.img = img
+        self.brush = brush
         self.set_callback()
-                
-
+        self.add_trackbars()
+        
     def display(self):
         cv2.imshow(self.name, self.img)
-
+        
 
     def callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            brush.draw(new_canvas.img, x, y)
-            brush.drawing = True
+            self.brush.draw(new_canvas.img, x, y)
+            self.brush.drawing = True
 
         elif event == cv2.EVENT_MOUSEMOVE:
-            if brush.drawing:
-                brush.draw(new_canvas.img, x, y)
+            if self.brush.drawing:
+                self.brush.draw(new_canvas.img, x, y)
         elif event == cv2.EVENT_LBUTTONUP:
-            brush.drawing = False
+            self.brush.drawing = False
  
         
     def set_callback(self):
         cv2.setMouseCallback(self.name, self.callback)
 
+    def empty_callback(self, x):
+        pass
+    
+    def add_trackbars(self):
+        cv2.createTrackbar('R', self.name, 0, 255, lambda x: None)
+        cv2.createTrackbar('G', self.name, 0, 255, lambda x: None)
+        cv2.createTrackbar('B', self.name, 0, 255, lambda x: None)
+        
+        
 class Brush:
 
     def __init__(self, color):
@@ -43,12 +53,16 @@ img = np.zeros((300, 512, 3), np.uint8)
 img[:] = [255, 255, 255]
 brush = Brush((255, 0, 0))
 new_canvas = Canvas('Image', img, brush)
-new_canvas.brush = brush
-brush.canvas = new_canvas
 
 
 while True:
     new_canvas.display()
+    r = cv2.getTrackbarPos('R', new_canvas.name)
+    g = cv2.getTrackbarPos('G', new_canvas.name)
+    b = cv2.getTrackbarPos('B', new_canvas.name)
+
+    brush.color = (r, g, b)
+    
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
