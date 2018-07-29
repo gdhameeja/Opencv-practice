f924f136dc83a31c1d75b092548d9774904349f1
Gaurav <gdhameeja@gmail.com>
2018-07-29 16:07:39 +0530

    Cant believe I did it..

--- cv-gui/cvlogoexcercise.py
+++ cv-gui/cvlogoexcercise.py
@@ -2,13 +2,27 @@ import cv2
 import numpy as np
 
 color = None
-mode = True # if true create triangle, else create a circle
+triangle_vertices = []
+mode = True  # if true create circle, else create a triangle
+
 # callback for the mouse click
 def callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x, y), 5, color, -1)
-                
-
+        if mode:
+            global triangle_vertices
+            triangle_vertices = []
+            cv2.circle(img, (x, y), 50, color, -1)
+        else:
+            # draw the triangle
+            global triangle_vertices
+            if len(triangle_vertices) != 3:
+                triangle_vertices.append([x, y])
+            else:
+                print(triangle_vertices)
+                pts = np.array(triangle_vertices)
+                pts = pts.reshape(3, 1, 2)
+                cv2.polylines(img, [pts], True, (255, 255, 255))
+                triangle_vertices = []
 
 cv2.namedWindow('OpenCVLogo')
 cv2.setMouseCallback('OpenCVLogo', callback)
@@ -16,7 +30,6 @@ img = np.zeros((512, 512, 3), np.uint8)
 
 while True:
     cv2.imshow('OpenCVLogo', img)
-    global color
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
@@ -32,4 +45,30 @@ while True:
         color = (255, 255, 255)
                 
 cv2.destroyAllWindows()
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
